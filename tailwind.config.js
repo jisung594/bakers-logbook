1b8c5d8748f05d3167d3bdcaa83121d293da5322
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-10-26 11:36:04 -0500

    Adds config files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Below tells Tailwind where to scan for classes (HTML templates and TS component files)
+  content: [
+    "./src/**/*.{html,ts}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
