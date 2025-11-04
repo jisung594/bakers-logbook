0b0b652b3c86b14a63fe2c3ebfed829d44f04c31
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-04 09:23:11 -0600

    Updates with inline content from tailwind.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,25 @@
+// /** @type {import('postcss-load-config').Config} */
+// module.exports = {
+//   plugins: {
+//     // Note: We explicitly use the PostCSS plugin package here
+//     '@tailwindcss/postcss': {}, 
+//     autoprefixer: {},
+//   },
+// };
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    // Runs the Tailwind plugin and passes the config directly.
+    require('tailwindcss')({
+      // inlined content from the separate config file that was causing build to fail (tailwind.config.js)
+      content: [
+        "./src/**/*.{html,ts}",
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }),
+    require('autoprefixer'),
+  ],
+};
