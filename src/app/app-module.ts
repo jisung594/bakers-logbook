d6fbff4f768989b0179b1face70de29a86048e40
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-05 19:11:03 -0600

    Includes new component in ng declarations

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './header/header';
+import { RecipeForm } from './recipe-form/recipe-form';
 
 @NgModule({
   declarations: [
     App,
-    Header
+    Header,
+    RecipeForm
   ],
   imports: [
     BrowserModule,
