8014402fa4a88a2fe315ecee5c5ad5c37be8d7b0
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-05 19:09:21 -0600

    Creates recipe-form component

---
+++ src/app/recipe-form/recipe-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-form',
+  standalone: false,
+  templateUrl: './recipe-form.html',
+  styleUrl: './recipe-form.css',
+})
+export class RecipeForm {
+
+}
