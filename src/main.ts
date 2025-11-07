d4acb5cf74309dac33efe6d02a62d4a62a0953cf
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 08:54:53 -0600

    Boots standalone App instead and injects firebase/firestore

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
-import { platformBrowser } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+// import { platformBrowser } from '@angular/platform-browser';
+import { App } from './app/app';
 import { AppModule } from './app/app-module';
+import { importProvidersFrom } from '@angular/core';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
 
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+// platformBrowser().bootstrapModule(AppModule, {
+//   ngZoneEventCoalescing: true,
+// })
+//   .catch(err => console.error(err));
+
+bootstrapApplication(App, {
+  providers: [
+    // importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore())
+    // ),
+  ],
+});
