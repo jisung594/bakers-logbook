c24536c780d5a44ebbf30831121836650a6319d5
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 15:59:17 -0600

    Adds imports to root component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Header } from './header/header';
+import { IngredientsForm } from './recipe/ingredients-form/ingredients-form';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: true,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [CommonModule, Header, IngredientsForm],
 })
 export class App {
   protected readonly title = signal('bakers-logbook');
