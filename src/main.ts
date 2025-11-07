8d7dd15549ef5fa66ec462af0b4dacf8a0b150d4
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 15:57:15 -0600

    Removes ngmodule bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-// import { platformBrowser } from '@angular/platform-browser';
 import { App } from './app/app';
-import { AppModule } from './app/app-module';
-import { importProvidersFrom } from '@angular/core';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 
-// platformBrowser().bootstrapModule(AppModule, {
-//   ngZoneEventCoalescing: true,
-// })
-//   .catch(err => console.error(err));
-
 bootstrapApplication(App, {
   providers: [
-    // importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-      provideFirestore(() => getFirestore())
-    // ),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
 });
