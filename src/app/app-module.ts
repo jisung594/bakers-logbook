0d5abe26babc4012a20fd58c7ae031434653720d
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 14:49:05 -0600

    Renames RecipeForm to IngredientsForm (now under /recipe)

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './header/header';
-import { RecipeForm } from './recipe-form/recipe-form';
+import { IngredientsForm } from './recipe/ingredients-form/ingredients-form';
 // import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 // import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
@@ -11,15 +11,13 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    App,
-    // Header
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    App,
     Header,
-    RecipeForm,
+    IngredientsForm,
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
     // provideFirestore(() => getFirestore())
     AngularFireModule.initializeApp(environment.firebaseConfig),
