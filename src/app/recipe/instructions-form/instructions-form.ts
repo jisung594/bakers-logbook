f2087eccd83fc2c04f86b766224ea950b070c16f
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 16:29:14 -0600

    Creates instructions-form component / adds basic markup

---
+++ src/app/recipe/instructions-form/instructions-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructions-form',
+  imports: [],
+  templateUrl: './instructions-form.html',
+  styleUrl: './instructions-form.css'
+})
+export class InstructionsForm {
+
+}
