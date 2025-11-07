49e3df23c98193c8918a80114ac3467e8e8de486
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 08:43:14 -0600

    Makes Header standalone

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: false, // why does setting to 'true' cause build to fail?
+  standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
