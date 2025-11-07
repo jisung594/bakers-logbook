61f270ba74319e86b0a9525a07d5f0343d5bf52f
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 16:02:50 -0600

    commenting out app-module.ts for now

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,32 +1,32 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
-import { Header } from './header/header';
-import { IngredientsForm } from './recipe/ingredients-form/ingredients-form';
-// import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-// import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { environment } from '../environments/environment';
+// import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppRoutingModule } from './app-routing-module';
+// import { App } from './app';
+// import { Header } from './header/header';
+// import { IngredientsForm } from './recipe/ingredients-form/ingredients-form';
+// // import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+// // import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+// import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+// import { environment } from '../environments/environment';
 
-@NgModule({
-  declarations: [],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    App,
-    Header,
-    IngredientsForm,
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideFirestore(() => getFirestore())
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     App,
+//     Header,
+//     IngredientsForm,
+//     // provideFirebaseApp(() => initializeApp(environment.firebase)),
+//     // provideFirestore(() => getFirestore())
+//     AngularFireModule.initializeApp(environment.firebaseConfig),
+//     AngularFirestoreModule
 
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
-})
-export class AppModule { }
+//   ],
+//   providers: [
+//     provideBrowserGlobalErrorListeners()
+//   ],
+//   bootstrap: [App]
+// })
+// export class AppModule { }
