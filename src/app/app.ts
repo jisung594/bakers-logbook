c76d0773e40ce7888e443f7d1097d5398bb8beae
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 17:13:13 -0600

    Adds instructions-form to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,19 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Header } from './header/header';
 import { IngredientsForm } from './recipe/ingredients-form/ingredients-form';
+import { InstructionsForm } from './recipe/instructions-form/instructions-form';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css',
-  imports: [CommonModule, Header, IngredientsForm],
+  imports: [
+    CommonModule, 
+    Header, 
+    IngredientsForm, 
+    InstructionsForm
+  ],
 })
 export class App {
   protected readonly title = signal('bakers-logbook');
