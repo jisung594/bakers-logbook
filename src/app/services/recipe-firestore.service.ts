911493cf7fcd8f0f3a77dceaa411a0e8064e4641
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-07 08:47:09 -0600

    Creates firestore service with add/get functions

--- src/app/services/recipe-firestore.service.ts
+++ src/app/services/recipe-firestore.service.ts
@@ -1,3 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Recipe } from '../models/recipe.model';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeFirestoreService {
+  constructor(private firestore: Firestore) {}
 
-// RecipeFirestoreService
+  addRecipe(recipe: Recipe) {
+    const recipesRef = collection(this.firestore, 'recipes');
+    return addDoc(recipesRef, recipe);
+  }
+
+  getRecipes(): Observable<Recipe[]> {
+    const recipesRef = collection(this.firestore, 'recipes');
+    return collectionData(recipesRef, { idField: 'id' }) as Observable<Recipe[]>;
+  }
+}
