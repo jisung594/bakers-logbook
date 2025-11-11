26b847770567ecd21b72b2ef087903564e714856
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-11 08:12:13 -0600

    Sets up new parent component to save data instead of children (wip)

--- src/app/recipe/recipe-form/recipe-form.ts
+++ src/app/recipe/recipe-form/recipe-form.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IngredientsForm } from './ingredients-form/ingredients-form';
+import { InstructionsForm } from './instructions-form/instructions-form';
+import { RecipeFirestoreService } from '../../services/recipe-firestore.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-form',
-  imports: [],
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    IngredientsForm, 
+    InstructionsForm
+  ],
   templateUrl: './recipe-form.html',
   styleUrl: './recipe-form.css'
 })
 export class RecipeForm {
+  recipeName = '';
+  ingredients: any[] = [];
+  instructions: any[] = [];
 
+  constructor(private recipeRepo: RecipeFirestoreService) {}
+
+  // =============================================
+  // (will) listen for changes in both child forms
+  onIngredientsChange(ingredients: any[]) {
+    this.ingredients = ingredients;
+  }
+
+  onInstructionsChange(instructions: any[]) {
+    this.instructions = instructions;
+  }
+  // =============================================
+
+  async saveRecipe(index: number) {
+
+    // Exits if recipe name field is empty
+    if (!this.recipeName.trim()) return;
+
+    const newRecipe = {
+      name: this.recipeName,
+      ingredients: this.ingredients,
+      instructions: this.instructions
+    }
+
+    
+
+  }
 }
