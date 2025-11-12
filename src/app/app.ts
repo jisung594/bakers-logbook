87e7c97f72031ed5ce7c7a3483d6292a31206dab
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-12 13:19:50 -0600

    Removes child components from app imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Header } from './header/header';
-import { IngredientsForm } from './recipe/recipe-form/ingredients-form/ingredients-form';
-import { InstructionsForm } from './recipe/recipe-form/instructions-form/instructions-form';
 import { RecipeForm } from './recipe/recipe-form/recipe-form';
 
 @Component({
@@ -12,9 +10,7 @@ import { RecipeForm } from './recipe/recipe-form/recipe-form';
   styleUrl: './app.css',
   imports: [
     CommonModule, 
-    Header, 
-    IngredientsForm, 
-    InstructionsForm,
+    Header,
     RecipeForm,
   ],
 })
