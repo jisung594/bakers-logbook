d4fbb0e621f5c0aa463a422990c57560766dcfbd
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-14 13:34:15 -0600

    Imports and injects auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Auth {
-  
+export class AuthService {
+  constructor(private auth: Auth) {}
 }
