a54dae1232b08cebe02be28a74aabaa7e0a8f0d9
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-14 13:55:47 -0600

    Adds google sign-in functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
+import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   constructor(private auth: Auth) {}
+
+  async signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+
+    // built-in Firebase function for handling signin via Google's popup window
+    return signInWithPopup(this.auth, provider);
+  }
 }
