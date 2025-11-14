52011f4a1889139bc79daa26714a7c78e3ffba01
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-13 20:14:11 -0600

    Adds logic for updating existing docs vs creating new ones

--- src/app/recipe/recipe-form/recipe-form.ts
+++ src/app/recipe/recipe-form/recipe-form.ts
@@ -23,6 +23,7 @@ export class RecipeForm {
   };
   ingredients: any[] = [];
   instructions: any[] = [];
+  currentRecipeId: string | null = null;
 
   constructor(private recipeRepo: RecipeFirestoreService) {}
 
@@ -43,19 +44,49 @@ export class RecipeForm {
   }
 
   async saveRecipe() {
-    if (this.recipeName && (this.ingredients.length || this.instructions.length)) {
-      const newRecipe = {
+    // Requires at least a valid recipe name upon submit
+    if (this.recipeName) {
+      const recipeData = {
         name: this.recipeName.value,
         ingredients: this.ingredients,
         instructions: this.instructions,
+        updatedAt: new Date(),
       }
 
       try {
-        await this.recipeRepo.addRecipe(newRecipe);
-        console.log('Recipe saved successfully:', newRecipe);
+        // Updates firestore doc directly if existing recipe
+        if (this.currentRecipeId) {
+          await this.recipeRepo.updateRecipe(this.currentRecipeId, recipeData);
+          console.log('Recipe updated successfully.')
+          return;
+        } 
+        
+
+        // Checks if recipe already exists by name
+        const existingRecipeDoc = await this.recipeRepo.getRecipeByName(recipeData.name);
+
+        // (if it does) Sets current id to that of found doc in firestore
+        if (!existingRecipeDoc.empty) {
+          this.currentRecipeId = existingRecipeDoc.docs[0].id;
+          await this.recipeRepo.updateRecipe(this.currentRecipeId, recipeData);
+          console.log('Existing recipe updated successfully.');
+        } else {
+          const newDocRef = await this.recipeRepo.addRecipe({
+            ...recipeData, 
+            createdAt: new Date(),
+          });
+
+          this.currentRecipeId = newDocRef.id;
+          console.log('Recipe created successfully.');
+        }
+
+        
+
       } catch (err) {
         console.error('Error saving recipe:', err);
       }
+    } else {
+      console.warn('Recipe name is required before saving.');
     }
   }
 }
