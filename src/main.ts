03de5204d8266adecde3db42c35890e0f27d0874
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-13 23:17:13 -0600

    Injects firebase auth dependency

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 
 bootstrapApplication(App, {
   providers: [
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
 });
