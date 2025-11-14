4f05ce047398c4381e6888ddc02394141ede239c
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-14 15:24:33 -0600

    Adds click handler for logging with google

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  standalone: false,
+  standalone: true,
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  constructor(private authService: AuthService) {}
 
+  async handleGoogleSignIn() {
+    try {
+      const userCreds = await this.authService.signInWithGoogle();
+      console.log("Logged in as", userCreds.user);
+    } catch (err) {
+      console.log("Login error:", err);
+    }
+  }
 }
