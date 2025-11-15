7213685035bbfffc163e89a3475efb165d25f7df
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-15 11:35:42 -0600

    Listens for changes, then updates user var

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements OnInit {
+  user: User | null = null;
+
   constructor(private authService: AuthService) {}
+
+  // Called once when component is ready
+  ngOnInit() {
+    this.authService.authState$.subscribe(user => {
+      this.user = user;
+    });
+  }
 
   async handleGoogleSignIn() {
     try {
