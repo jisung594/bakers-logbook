75fc71c5a31707398e8ac3aee68352a894ad4388
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-17 19:24:39 -0600

    Enables routing

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { App } from './app/app';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideRouter } from '@angular/router';
 import { environment } from './environments/environment';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
   providers: [
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
+    provideRouter(routes)
   ],
 });
