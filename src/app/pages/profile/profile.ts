527dfc4f90ff4b842fe0fe54d7415210dc49991f
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-17 18:48:32 -0600

    Ensures user's recipe list is ready before component is rendered

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recipe } from '../../models/recipe.model';
+import { AuthService } from '../../services/auth.service';
+import { RecipeFirestoreService } from '../../services/recipe-firestore.service';
 
 @Component({
   selector: 'app-profile',
-  standalone: false,
+  standalone: true,
+  imports:[CommonModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
 export class Profile {
+  // $ (syntax for observable)
+  // ! (not initialized in constructor, but
+  //   promises to assign value before accessed)
+  recipes$!: Observable<Recipe[]>;
 
+  constructor(
+    private authService: AuthService,
+    private firestoreService: RecipeFirestoreService
+  ) {}
+
+  // Called after input properties are set, but before DOM is ready
+  async ngOnInIt() {
+    const user = this.authService.getCurrentUser();
+
+    if (user) {
+      this.recipes$ = this.firestoreService.getUserRecipes(user.uid);
+    }
+  }
 }
