e320771a38bd88ee72424fbedfec368d74c714f0
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-18 15:22:04 -0600

    Adds a /recipe route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    // {
+    //     path: '',
+    //     // Lazy loads the Home component
+    //     loadComponent: () =>
+    //         import('./pages/home/home').then(module => module.Home)
+    // },
     {
+        path: 'recipe',
+        // Lazy loads the Recipe component
+        loadComponent: () =>
+            import('./pages/recipe/recipe').then(module => module.Recipe)
+    },
+        {
         path: 'profile',
         // Lazy loads the Profile component
         loadComponent: () =>
