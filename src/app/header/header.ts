7b1889099001d9adb1643d79426494ab359d7243
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-11-18 10:19:54 -0600

    Updates required imports for profile link

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
 import { Login } from '../auth/login/login';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [Login],
+  imports: [
+    Login, 
+    CommonModule, 
+    RouterModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  // user$ = this.authService.authState$;
+  user$!: Observable<User | null>;
 
+  constructor(private authService: AuthService) {
+    this.user$ = this.authService.authState$;
+  }
 }
