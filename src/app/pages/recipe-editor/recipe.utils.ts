db34e5613db306941bc65605d84a3b7762db2f71
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-12-03 08:43:45 -0600

    Adds utils file w/ functions for converting form data into firestore objects

---
+++ src/app/pages/recipe-editor/recipe.utils.ts
@@ -0,0 +1,20 @@
+import { Ingredient, Instruction } from '../../models/recipe.model';
+import { IngredientRow } from './ingredients-form/ingredients-form.types';
+import { InstructionRow } from './instructions-form/instructions-form.types';
+
+// Transforms array of IngredientRow FormGroup's into Firestore-compatible Ingredient objects
+export function mapIngredientRows(rows: IngredientRow[]): Ingredient[] {
+  return rows.map((row) => ({
+    name: row.controls.name.value,
+    quantity: row.controls.quantity.value,
+    unit: row.controls.unit.value,
+  }));
+}
+
+// Transforms array of InstructionRow FormGroup's into Firestore-compatible Instruction objects
+export function mapInstructionRows(rows: InstructionRow[]): Instruction[] {
+  return rows.map((row) => ({
+    step: row.controls.step.value,
+    order: row.controls.order.value,
+  }));
+}
